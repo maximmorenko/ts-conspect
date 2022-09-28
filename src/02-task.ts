af10f4dad617e98710957c602302ed9e5c73f445
Maksym Morenko <morenko.maxim@gmail.com>
2022-09-28 03:11:19 +0300

    Задача переписать функцию на TypeScript

---
+++ src/02-task.ts
@@ -0,0 +1,20 @@
+// Дана JavaScript функция. Необходимо переписать ее на TypeScript.
+
+function slice(str: string, start: number, end?: number): string {
+    let newStr = '';
+  
+    let lastIndex: number;
+  
+    if (end) {
+        lastIndex = end > str.length ? str.length : end;
+    } else {
+        lastIndex = str.length;
+    }
+  
+    for (let i = start; i < lastIndex; i++) {
+        newStr += str[i];
+    }
+  
+    return newStr;
+}
+slice('какая-то строка', 9, 13)
