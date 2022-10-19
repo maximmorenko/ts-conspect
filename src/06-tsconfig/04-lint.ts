60d0907c62afe0b0120a05dd4fb0d6e91dba2a28
Maksym Morenko <morenko.maxim@gmail.com>
2022-10-19 15:59:26 +0300

    Дополнительный линтинг в конфиге

---
+++ src/06-tsconfig/04-lint.ts
@@ -0,0 +1,14 @@
+// Дополнительный линтинг в конфиге
+// по умолчанию все опции отключены
+
+// "noPropertyAccessFromIndexSignature": true, // не позволяет обращаться к необъявленым типам
+
+// "noUncheckedIndexedAccess": true, // обязует делать проверку на то, что поле существует
+
+// "noImplicitReturns": true, // нет неявному ретерну
+
+// "noFallthroughCasesInSwitch": true, // обязует в свиче указывать break
+
+// "noUnusedLocals": true, // обязует использовать все объявленные локальные переменные
+
+// "noUnusedParameters": true, // обязует использовать все объявленные параметры в функции
